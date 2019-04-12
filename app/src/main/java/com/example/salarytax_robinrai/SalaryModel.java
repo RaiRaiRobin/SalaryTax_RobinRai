6118b8b518fd652666f6fbf546fb1189f39fe4b5
RaiRaiRobin <rai.ribin1000@gmail.com>
2019-04-12 21:18:53 +0545

    Salary Tax

---
+++ app/src/main/java/com/example/salarytax_robinrai/SalaryModel.java
@@ -0,0 +1,16 @@
+package com.example.salarytax_robinrai;
+
+public class SalaryModel {
+
+    float salary;
+    public SalaryModel(float salary) {
+        this.salary = salary;
+    }
+    public float getSalary() {
+        return salary;
+    }
+    public void setSalary(float salary) {
+        this.salary = salary;
+    }
+
+}
